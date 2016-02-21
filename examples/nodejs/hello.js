b3a7d32a64eab36ba68c65ac28260e774613923e
Xiwen Cheng <xiwen.cheng@mendix.com>
2016-02-21 12:29:50 +0100

    add nodejs example

---
+++ examples/nodejs/hello.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var port = parseInt(process.env.PORT, 10);
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello there world\n');
+}).listen(port, '0.0.0.0');
+console.log('Serverning at http://0.0.0.0:' + port.toString() + '/');
