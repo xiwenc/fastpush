792d4e3b3120f16107e6abb6d13b0c2454db313c
Xiwen Cheng <xiwen.cheng@mendix.com>
2016-06-23 14:59:32 +0200

    improve python example

--- examples/python/hello.py
+++ examples/python/hello.py
@@ -8,13 +8,23 @@ port = int(os.getenv("PORT", 9099))
 
 @app.route('/')
 def hello_world():
-    return 'Hello there World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="sample.css">
+    </head>
+    <body>
+        <h1>Hello there World</h1>
+    </body>
+</html>
+"""
 
 
 @app.route('/sample.css')
 def static_proxy():
     with open('sample.css') as f:
-        return f.read()
+        return f.read(), 200, {'Content-Type': 'text/css; charset=utf-8'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
