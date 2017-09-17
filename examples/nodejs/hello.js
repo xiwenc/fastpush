b8395bbed986b0d4a3a4593ab11b438b225ed5e0
Xiwen Cheng <xiwen.cheng@mendix.com>
2017-09-17 11:44:51 +0200

    example: nodejs

--- examples/nodejs/hello.js
+++ examples/nodejs/hello.js
@@ -4,4 +4,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello there world\n');
 }).listen(port, '0.0.0.0');
-console.log('Serverning at http://0.0.0.0:' + port.toString() + '/');
+console.log('Serving at http://0.0.0.0:' + port.toString() + '/');
